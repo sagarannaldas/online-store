2f8d4882b49408f68c3a8b8fce9180130a382135
sagarannaldas <annal7057@gmail.com>
2026-01-07 19:38:00 +0530

    1. Dependency injection 2. Beans Configuration via annotations and code 3. Added JDBC driver 4. Database scheme creation for users and addresses 5. flyway integration for database migration control

--- src/main/java/com/sagarannaldas/online_store/OnlineStoreApplication.java
+++ src/main/java/com/sagarannaldas/online_store/OnlineStoreApplication.java
@@ -1,13 +1,25 @@
 package com.sagarannaldas.online_store;
 
+import com.sagarannaldas.online_store.user.User;
+import com.sagarannaldas.online_store.user.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class OnlineStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(OnlineStoreApplication.class, args);
+//		var orderService = context.getBean(OrderService.class);
+//		var notificationService = context.getBean(NotificationManager.class);
+//        orderService.placeOrder();
+//		notificationService.sendNotification("0394302984", "hello world ");
+
+//		var userService = context.getBean(UserService.class);
+//		userService.registerUser(new User(1L,"abc@gmail.com", "1234","abcuser"));
+//		userService.registerUser(new User(1L,"abc@gmail.com", "1234","abcuser"));
+
+    }
 
 }
