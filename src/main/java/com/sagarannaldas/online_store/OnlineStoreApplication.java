6c964985eb3411d42068a573204ed2d86386702d
sagarannaldas <annal7057@gmail.com>
2026-01-10 08:13:31 +0530

    1. Added Lombok library for getter, setter, constructor, builder

--- src/main/java/com/sagarannaldas/online_store/OnlineStoreApplication.java
+++ src/main/java/com/sagarannaldas/online_store/OnlineStoreApplication.java
@@ -1,5 +1,6 @@
 package com.sagarannaldas.online_store;
 
+import com.sagarannaldas.online_store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +9,12 @@ import org.springframework.context.ApplicationContext;
 public class OnlineStoreApplication {
 
     public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(OnlineStoreApplication.class, args);
+        ApplicationContext context = SpringApplication.run(OnlineStoreApplication.class, args);
+        var user = User.builder()
+                .id(1L)
+                .email("email")
+                .password("password")
+                .build();
     }
 
 }
--- src/main/java/com/sagarannaldas/online_store/entities/User.java
+++ src/main/java/com/sagarannaldas/online_store/entities/User.java
@@ -1,8 +1,14 @@
 package com.sagarannaldas.online_store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "users")
 public class User {
     @Id
@@ -17,36 +23,4 @@ public class User {
 
     @Column(nullable = false, name = "password")
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
